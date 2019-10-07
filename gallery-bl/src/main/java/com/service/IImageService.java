e90701e84e17147a23029fc05a44d4813f855e18
acugu <acugurotis@gmail.com>
2019-10-07 11:09:53 +0300

    Test

--- gallery-bl/src/main/java/com/service/IImageService.java
+++ gallery-bl/src/main/java/com/service/IImageService.java
@@ -3,7 +3,6 @@ package com.service;
 import com.entity.Image;
 import com.payload.ImageUpdate;
 import com.payload.ImageUpload;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
